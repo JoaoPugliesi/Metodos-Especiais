96909714524ad4d7fa405ee17f4f7233d2e4f875
João <joaopaulo.allex@gmail.com>
2022-03-27 20:18:38 -0300

    Projeto POO

---
+++ src/Aula04.java
@@ -0,0 +1,14 @@
+
+public class Aula04 {
+
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta("Faber-Castell", "Vermelha", 1.0f);
+		c1.status(); 
+		
+		Caneta c2 = new Caneta("COMPACTOR", "Preta", 0.5f);
+		c2.status();
+		
+
+	}
+
+}
---
+++ src/Caneta.java
@@ -0,0 +1,42 @@
+
+public class Caneta {
+	public String modelo;
+	private float ponta;
+	private String cor;
+	private boolean tampada;
+	
+	public Caneta (String m, String c, float p) {// Este é o método construtor
+		this.modelo = m;
+		this.cor = c;
+		this.ponta = p;
+		this.destampar();
+	}
+	
+	public String getModelo() {
+		return this.modelo;
+	}
+	public void setModelo(String m) {
+		this.modelo = m;
+	}
+	public float getPonta() {
+		return this.ponta;
+	}
+	public void setPonta(float p) {
+		this.ponta = p;
+	}
+	public void tampar( ) {
+		this.tampada = true;
+	}
+	public void destampar() {
+		this.tampada = false;
+	}
+	public void status() {
+		System.out.println("SOBRE A CANETA:");
+		System.out.println("Modelo: " + this.modelo);
+		System.out.println("Ponta: " + this.ponta);
+		System.out.println("Cor: " + this.cor);
+		System.out.println("Está tampada? " + this.tampada);
+		System.out.println(" ");
+	}
+
+}
